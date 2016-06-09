3dd15e7cad8111e5267d4d63e283ea2ca1cc0bda
Bas Schouten <bas@roomerpms.com>
2016-06-09 08:47:21 +0200

    Merged branch RPMS-336 into RPMS-336 Merged branch master into RPMS-336 Merged branch master into master Fixed memory leak with THotelConfigEntities Fixed memory leak with array of HotelConfigEntitiies Merged branch master into RPMS-336

--- uItems2.pas
+++ uItems2.pas
@@ -1057,13 +1057,15 @@ end;
 function TfrmItems2.AssertCorrectness : Boolean;
 var config : Array_Of_THotelconfigurationsEntity;
     answer : Integer;
+    i: integer;
 begin
 
   result := true;
   if Lookup then
     exit;
+
+  config := d.roomerMainDataSet.Hotelconfigurations_Entities_FindAll;
   try
-    config := d.roomerMainDataSet.Hotelconfigurations_Entities_FindAll;
     if config[0].forceExternalProductIdCorrectness = 1 then
     begin
       m_Items.DisableControls;
@@ -1096,7 +1098,9 @@ begin
         m_Items.EnableControls;
       end;
     end;
-  except
+  finally
+    for i := 0 to length(config)-1 do
+      config[i].Free;
   end;
 end;
 
