5a44e3550411df43d3c972ec114ebb0d0732fdcd
Bas Schouten <bas@roomerpms.com>
2016-10-31 18:40:06 +0100

    Removed duplicates and unused folders

--- RoomerPMSRelease/GoDaddyCertificate/DeployRoomerExeOnly.bat
+++
@@ -1,8 +0,0 @@
-﻿
-
-signtool sign /f GoDaddyCertificate\roomerpms.pfx /p Baslari.0 /t http://timestamp.verisign.com/scripts/timestamp.dll Roomer.exe
-
-AWSCLI\aws.exe configure<PRODIAM
-
-AWSCLI\aws.exe s3 cp Roomer.exe s3://roomerstore.com/Roomer.exe
-AWSCLI\aws.exe s3 cp s3://roomerstore.com/Roomer.exe s3://roomerstore.com/roomer.exe
